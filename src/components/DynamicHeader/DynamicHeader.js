55cc1e9bf56255d2d8c5f8dcebfa63fef1dbebac
OFFICE\dmytro.zhuk <dmytro.zhuk@listat.com.ua>
2018-10-18 17:33:11 +0300

    Changed cards style.

--- src/components/DynamicHeader/DynamicHeader.js
+++ src/components/DynamicHeader/DynamicHeader.js
@@ -55,7 +55,7 @@ class DynamicHeader extends Component {
       for (let i = 0; i < count; i++) {
         result.push(
             <div key={i} className="ticket-item">
-                <img style={{transform: `rotate(${270 + delta*(itemIndex - Math.floor(data.gamesTypesCount/2))+ i*2}deg)`, left: (delta < 30 ? 30 : delta)*itemIndex, bottom: -60 - itemIndex*30}} src={`http://images.jinnilotto.com/lp/scratchcards/${item.name}.png`} alt="pick" />
+                <img style={{transform: `rotate(${270 + delta/1.2*(itemIndex - Math.floor(data.gamesTypesCount/2))+ i*2}deg)`, left: (data.gamesTypesCount < 6 ? 80 : delta)*itemIndex, bottom: -60 - itemIndex*30}} src={`http://images.jinnilotto.com/lp/scratchcards/${item.name}.png`} alt="pick" />
             </div>
         )
       }
