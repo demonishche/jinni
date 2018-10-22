1888ac1e3153ca29241c2151bf021051423ec99b
OFFICE\dmytro.zhuk <dmytro.zhuk@listat.com.ua>
2018-10-22 09:59:38 +0300

    Changed stage detect.

--- src/tools/envSettings.js
+++ src/tools/envSettings.js
@@ -1,4 +1,6 @@
 const liveHost = "lp.jinnilotto.com";
+const stageHost = "stage";
+const localhost = "localhost";
 
 const liveApi = "api.jinnilotto.com";
 const stageApi = "stage-api.jinnilotto.com";
@@ -22,5 +24,5 @@ const stageSettings = {
     lottoParamsData: stageLottoParamsData
 }
 
-module.exports = window.location.hostname.includes(liveHost) ? liveSettings : stageSettings;
+module.exports = (window.location.hostname.includes(stageHost) || window.location.hostname.includes(localhost)) ? stageSettings : liveSettings;
 // module.exports = liveSettings;
